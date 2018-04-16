9d09e7b2b3fdca49d937dc6b78003ea6b6644c7d
kz56cd <kz56cd@gmail.com>
2018-04-16 15:28:56 +0900

    Init project

---
+++ SearchByReactorKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchByReactorKit
+//
+//  Created by Masakazu Sano on 2018/04/16.
+//  Copyright © 2018年 Masakazu Sano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
