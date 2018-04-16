f1103d319cbcc09929fbd8c6ae6889af23f861b0
kz56cd <kz56cd@gmail.com>
2018-04-16 17:14:20 +0900

    Update VC and reactor

--- SearchByReactorKit/GitHubSearchViewController.swift
+++ SearchByReactorKit/GitHubSearchViewController.swift
@@ -20,9 +20,65 @@ class GitHubSearchViewController: UIViewController, StoryboardView {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        prepareView()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        UIView.setAnimationsEnabled(false)
+        searchController.isActive = true
+        searchController.isActive = false
+        UIView.setAnimationsEnabled(true)
+    }
+}
+
+extension GitHubSearchViewController {
+    fileprivate func prepareView() {
+        tableView.scrollIndicatorInsets.top = tableView.contentInset.top
+        searchController.dimsBackgroundDuringPresentation = false
+        navigationItem.searchController = searchController
+    }
+    
+    // MARK - ReactorKit
     func bind(reactor: GitHubSearchViewReactor) {
-        // wip
+        // Action
+        searchController.searchBar.rx.text
+            .throttle(0.3, scheduler: MainScheduler.instance)
+            .map { Reactor.Action.updateQuery($0) }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        tableView.rx.contentOffset
+            .filter { [weak self] offset in
+                guard let `self` = self,
+                    self.tableView.frame.height > 0 else {
+                        return false
+                }
+                return offset.y + self.tableView.frame.height >= self.tableView.contentSize.height - 100
+            }
+            .map { _ in Reactor.Action.loadNextPage }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        // State
+        reactor.state
+            .map { $0.repos }
+            .bind(to: tableView.rx.items(cellIdentifier: "cell")) { indexPath, repo, cell in
+                cell.textLabel?.text = repo
+            }
+            .disposed(by: disposeBag)
+        
+        // View
+        tableView.rx.itemSelected
+            .subscribe(onNext: { [weak self, weak reactor] indexPath in
+                guard let `self` = self else { return }
+                self.view.endEditing(true)
+                self.tableView.deselectRow(at: indexPath, animated: false)
+                guard let repo = reactor?.currentState.repos[indexPath.row] else { return }
+                guard let url = URL(string: "https://github.com/\(repo)") else { return }
+                let viewController = SFSafariViewController(url: url)
+                self.searchController.present(viewController, animated: true, completion: nil)
+            })
+            .disposed(by: disposeBag)
     }
 }
--- SearchByReactorKit/GitHubSearchViewReactor.swift
+++ SearchByReactorKit/GitHubSearchViewReactor.swift
@@ -12,27 +12,59 @@ import RxSwift
 
 final class GitHubSearchViewReactor: Reactor {
     enum Action {
-        // stub
+        case updateQuery(String?)
+        case loadNextPage
     }
     
     enum Mutation {
-        // stub
+        case setQuery(String?)
+        case setRepos([String], nextPage: Int?)
+        case appendRepos([String], nextPage: Int?)
+        case setLoadingNextPage(Bool)
     }
     
     struct State {
         var query: String?
-        // wip
+        var repos: [String] = []
+        var nextPage: Int?
+        var isLoadingNextPage: Bool = false
     }
     
     let initialState = State()
     
     func mutate(action: GitHubSearchViewReactor.Action) -> Observable<GitHubSearchViewReactor.Mutation> {
         
-        // WIP
+        // wip
         
+        switch action {
+        case let .updateQuery(query):
+            break
+        case .loadNextPage:
+            break
+        }
         return Observable.empty()
     }
     
-//    func reduce(state: GitHubSearchViewReactor.State, mutation: GitHubSearchViewReactor.Mutation) -> GitHubSearchViewReactor.State {
-//    }
+    func reduce(state: GitHubSearchViewReactor.State, mutation: GitHubSearchViewReactor.Mutation) -> GitHubSearchViewReactor.State {
+        switch mutation {
+        case let .setQuery(query):
+            var newState = state
+            newState.query = query
+            return newState
+        case let .setRepos(repos, nextPage):
+            var newState = state
+            newState.repos = repos
+            newState.nextPage = nextPage
+            return newState
+        case let .appendRepos(repos, nextPage):
+            var newState = state
+            newState.repos.append(contentsOf: repos)
+            newState.nextPage = nextPage
+            return newState
+        case let .setLoadingNextPage(isLoadingNextPage):
+            var newState = state
+            newState.isLoadingNextPage = isLoadingNextPage
+            return newState
+        }
+    }
 }
